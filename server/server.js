70c74dddf121155b74a07376505f3e6f49bf7114
lopezfelixisc <lopezfelix.isc@gmail.com>
2019-09-22 22:52:00 -0600

    npm listo

--- server/server.js
+++ server/server.js
@@ -47,3 +47,27 @@ app.delete('/usuario', function(req, res) {
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando en el puerto ${process.env.PORT}`);
 });
+
+//Repositorio github
+/**
+ * Crear repo en git
+ * git init 
+ * crear gitignore
+ * excluir node_modules
+ * git status
+ * git add .
+ * git commit -m "commit inicial"
+ * git remote add origin (ruta del proyecto git)
+ * git push -u origin master
+ * git tag -a v0.0.1 -m "Inicio de proyecto"  -> para crear tag de la version  del proyecto
+ * git push --tags
+ */
+
+
+/**
+ * Heroku
+ * Instalar heroku cli ->npm install -g heroku
+ * heroku login
+ * heroku create
+ * git push heroku master
+ */
